171875b00727f65215f728454574dd08e34113b9
Ivann <ivann.galic@edu.itescia.fr>
2020-03-30 16:48:02 +0200

    liens vers toutes les activities via la page HOME

--- ApplicationTvGoodGit/app/src/main/java/com/ivann/applicationtvgoodgit/MainActivity.java
+++ ApplicationTvGoodGit/app/src/main/java/com/ivann/applicationtvgoodgit/MainActivity.java
@@ -2,7 +2,10 @@ package com.ivann.applicationtvgoodgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textViewCollections = findViewById(R.id.textViewCollections);
+        textViewCollections.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCollections();
+            }
+        });
+
+        final TextView textViewRecherche = findViewById(R.id.textViewRecherche);
+        textViewRecherche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSearch();
+            }
+        });
+
+        final TextView textViewNouveautés = findViewById(R.id.textViewNouveautés);
+        textViewNouveautés.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToNewFilms();
+            }
+        });
+
+        final TextView textViewAPropos = findViewById(R.id.textViewAPropos);
+        textViewAPropos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAbout();
+            }
+        });
+    }
+
+    private void goToCollections() {
+        final Intent intentCollections = new Intent(this, CollectionsActivity.class);
+        startActivity(intentCollections);
+    }
+
+    private void goToSearch() {
+        final Intent intentSearch = new Intent(this, SearchActivity.class);
+        startActivity(intentSearch);
+    }
+
+    private void goToNewFilms() {
+        final Intent intentNewFilms = new Intent(this, NewFilmsActivity.class);
+        startActivity(intentNewFilms);
+    }
+
+    private void goToAbout() {
+        final Intent intentAbout = new Intent(this, AboutActivity.class);
+        startActivity(intentAbout);
     }
 }
