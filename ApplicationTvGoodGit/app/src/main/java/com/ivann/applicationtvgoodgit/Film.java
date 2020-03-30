f0462e724c3706584cd1f52087baf80361963931
cassel boris <casselboris92@gmail.com>
2020-03-30 22:45:23 +0200

    Classe film V1-sans casting ni real

---
+++ ApplicationTvGoodGit/app/src/main/java/com/ivann/applicationtvgoodgit/Film.java
@@ -0,0 +1,103 @@
+package com.ivann.applicationtvgoodgit;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/*
+cette classe permet de créer l'objet film à partir des données récupérées sur les api.
+Il est parcelisé pour pourvoir passer d'une vue à l'autre. Pour l'instant, pas de casting, ni de real.
+ */
+public class Film implements Parcelable  {
+
+    public final int idFilm;
+    public final String filmImage;
+    public final String titre;
+    public final String dateSortie;
+    public final int idGenre;
+    public final String resume;
+    public final float popularite;
+
+// ----------------------------------- CONSTRUCTEUR ---------------------------------------------------------------------- //
+    public Film(int idFilm, String filmImage, String titre, String dateSortie, int idGenre, String resume, float popularite) {
+        this.idFilm = idFilm;
+        this.filmImage = filmImage;
+        this.titre = titre;
+        this.dateSortie = dateSortie;
+        this.idGenre = idGenre;
+        this.resume = resume;
+        this.popularite = popularite;
+    }
+
+// ----------------------------------- PARCELISATION ---------------------------------------------------------------------- //
+
+    protected Film(Parcel in) {
+        idFilm = in.readInt();
+        filmImage = in.readString();
+        titre = in.readString();
+        dateSortie = in.readString();
+        idGenre = in.readInt();
+        resume = in.readString();
+        popularite = in.readFloat();
+    }
+
+    public static final Creator<Film> CREATOR = new Creator<Film>() {
+        @Override
+        public Film createFromParcel(Parcel in) {
+            return new Film(in);
+        }
+
+        @Override
+        public Film[] newArray(int size) {
+            return new Film[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(idFilm);
+        parcel.writeString(filmImage);
+        parcel.writeString(titre);
+        parcel.writeString(dateSortie);
+        parcel.writeInt(idGenre);
+        parcel.writeString(resume);
+        parcel.writeFloat(popularite);
+    }
+
+
+
+ //--------------------------------- GETTERS----------------------------------------------------//
+    public int getIdFilm() {
+        return idFilm;
+    }
+
+    public String getFilmImage() {
+        return filmImage;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getDateSortie() {
+        return dateSortie;
+    }
+
+    public int getIdGenre() {
+        return idGenre;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public float getPopularite() {
+        return popularite;
+    }
+
+
+}
