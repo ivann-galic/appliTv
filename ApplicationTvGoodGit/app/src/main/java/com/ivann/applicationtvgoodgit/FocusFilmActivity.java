c0601ba6858f28b9cdf458850efa252c37018132
Ivann <ivann.galic@edu.itescia.fr>
2020-03-31 11:45:29 +0200

    test affichage infos d'un film

---
+++ ApplicationTvGoodGit/app/src/main/java/com/ivann/applicationtvgoodgit/FocusFilmActivity.java
@@ -0,0 +1,31 @@
+package com.ivann.applicationtvgoodgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FocusFilmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_focus_film);
+
+        Intent srcIntent = getIntent();
+        final ArrayList<Film> filmList = srcIntent.getParcelableArrayListExtra("filmList");
+        assert filmList != null;
+        Log.i("FocusActivity", "la list des films " + filmList);
+/*        final TextView textViewTitle = findViewById(R.id.textViewTitle);
+        textViewTitle.setText(filmList.get(0).titre);*/
+        final TextView textViewParution = findViewById(R.id.textViewParution);
+        textViewParution.setText(filmList.get(0).dateSortie);
+        final TextView textViewResume = findViewById(R.id.textViewResume);
+        textViewResume.setText(filmList.get(0).resume);
+    }
+}
--- ApplicationTvGoodGit/app/src/main/java/com/ivann/applicationtvgoodgit/SearchActivity.java
+++ ApplicationTvGoodGit/app/src/main/java/com/ivann/applicationtvgoodgit/SearchActivity.java
@@ -1,11 +1,14 @@
 package com.ivann.applicationtvgoodgit;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
+import android.os.Parcel;
+import android.os.Parcelable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.jetbrains.annotations.NotNull;
@@ -25,88 +28,94 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 
+public class SearchActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        loadDataFromApi();
+    }
 
-    public class SearchActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_search);
-            loadDataFromApi();
-        }
-
-        private void loadDataFromApi() {
+    private void loadDataFromApi() {
         //    String userChoice = userchoice();
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url("https://api.themoviedb.org/3/discover/movie?api_key=d0f80747d8ac43db918936f4a3d09e9c&language=fr&sort_by=popularity.desc&page=1")
-                    .build();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/discover/movie?api_key=d0f80747d8ac43db918936f4a3d09e9c&language=fr&sort_by=popularity.desc&page=1")
+                .build();
 
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                    Log.e("MainActivity", "onFailue", e);
-                }
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e("MainActivity", "onFailue", e);
+            }
 
-                @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                    String body = response.body().string();
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String body = response.body().string();
 
-                    try {
+                try {
 
-                        JSONObject jsonBody = new JSONObject(body);
-                        JSONArray results = jsonBody.getJSONArray("results");
+                    JSONObject jsonBody = new JSONObject(body);
+                    JSONArray results = jsonBody.getJSONArray("results");
 
-                        ArrayList<Film> filmList = new ArrayList();
-                        // boucle pour enregistrer les films dans une liste
+                    ArrayList<Film> filmList = new ArrayList();
+                    // boucle pour enregistrer les films dans une liste
 
-                        for (int i=0; i <results.length();i++) {
+                    for (int i = 0; i < results.length(); i++) {
                         JSONObject film1 = results.getJSONObject(i);
                         //JSONObject film2 = results.getJSONObject(1);
 
                         int idFilm = film1.getInt("id");
-                        String filmImage = film1.getString("poster_path");
+                        String filmImageUrlTronquee = film1.getString("poster_path");
+                        String filmImage = "https://image.tmdb.org/t/p/original/" + filmImageUrlTronquee;
+                        Log.i("MainActivity", "filmImage" + filmImage);
+
                         String titre = film1.getString("original_title");
-                        String dateSortie =film1.getString("release_date");
+
+
+                        String dateSortie = film1.getString("release_date");
+
+
                         String resume = film1.getString("overview");
+
+
                         JSONArray idGenre = film1.getJSONArray("genre_ids");
-                        int idPremierGenre = (int)idGenre.get(0);
+                        int idPremierGenre = (int) idGenre.get(0);
                         float popularite = film1.getLong("popularity");
 
-                        Film film = new Film(idFilm,filmImage,titre,dateSortie,idPremierGenre,resume,popularite);
-                        filmList.add(film);
-                        }
+                        filmList.add(new Film(idFilm, filmImage, titre, dateSortie, idPremierGenre, resume, popularite));
+                    }
 
+                    Intent intent = new Intent(SearchActivity.this, FocusFilmActivity.class);
+                    intent.putParcelableArrayListExtra("FilmList", filmList);
 
-                        //Log.i("MainActivity", "resultat film1 page = 1 " + film1);
-                       // Log.i("MainActivity", "resultat film2 page = 1 " + film2);
-                       // Log.i ("MainActivity", "id du film 1 page = 1 " + idFilm);
-                        //Log.i("MainActivity","objet film : " + film.toString());
-                        Log.i("MainActivity","la list de sfilms "+filmList.toString());
-                       // System.out.println(film1.getString("title"));
+                    //Log.i("MainActivity", "resultat film1 page = 1 " + film1);
+                    // Log.i("MainActivity", "resultat film2 page = 1 " + film2);
+                    // Log.i ("MainActivity", "id du film 1 page = 1 " + idFilm);
+                    //Log.i("MainActivity","objet film : " + film.toString());
+                    Log.i("MainActivity", "la list de sfilms " + filmList.toString());
+                    // System.out.println(film1.getString("title"));
 
                        /* Movie movie1 = new Movie(film1.getString("title"),
                                                 film1.getString("title"),
 
                                 ))*/
 
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        e.getMessage();
-                    }
-                    //  System.out.println("je recup" + jsonArray[1]);
-                       // Log.i("MainActivity", "onResponse : body=" + body);
-
-                    //Log.i("MainActivity", "Started HTTP REquest");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    e.getMessage();
                 }
-            });
-        }
+                //  System.out.println("je recup" + jsonArray[1]);
+                // Log.i("MainActivity", "onResponse : body=" + body);
+
+                Log.i("MainActivity", "Started HTTP REquest");
+            }
+        });
+    }
 
 
-
-
-        // traitement de l'input de l'utilisateur
+    // traitement de l'input de l'utilisateur
 /*
         private String userChoice() {
             // je recupère l'entrée de l'utilisateur
@@ -116,6 +125,6 @@ import okhttp3.Response;
         */
 
 
-    }
+}
 
 
